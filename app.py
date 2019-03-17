fa98bae6f73eaef29798af84bcf14545a875f383
Jewell <wrightj16@southernct.edu>
2019-03-17 17:36:59 -0400

    Mini Update
    Added input handlers for the integer fields.
    Stylesheet still needs to be linked as a separate file.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, flash, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from config import Config
 from operations import Operations
 app = Flask(__name__)
 app.config['SECRET_KEY']='tempconfig'
+
+"""The app blah blah"""
 
 @app.route('/', methods=['GET', 'POST'])
 def index():	
@@ -17,7 +19,4 @@ def index():
 			return redirect(url_for('index'))
 	
 	return render_template("operations.html", output=output, form=form)
-
-	
-	
 	
